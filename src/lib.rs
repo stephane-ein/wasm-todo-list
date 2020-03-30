857515dd5aaf0ef2c2d1185ff4f075d288d56e49
Unknown <stephane.viper@gmail.com>
2020-03-30 16:00:19 +0200

    step 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
 extern crate wasm_bindgen;
 mod utils;
 use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+struct Todo {
+  name: String,
+}
+
+#[wasm_bindgen]
+struct Todos {
+  values: Vec<Todo>,
+}
+
+#[wasm_bindgen]
+impl Todos {
+  pub fn new() -> Todos {
+    let values = Vec::new();
+    Todos { values }
+  }
+
+  pub fn add_todo(&mut self, name: String) {
+    self.values.push(Todo { name });
+  }  
+
+  pub fn remove_todo(&mut self, id: usize) {
+    self.values.remove(id);
+  }  
+
+  pub fn get_todo(&mut self, id:usize) -> Todo {
+    let result = &self.values[id];
+    Todo { name: String::from(&result.name) }
+  }
+
+  pub fn get_todos_len(&mut self) -> usize {
+    self.values.len()
+  }
+}
