c26d3fa807a2bb29daba5368dc8c9d88c9cfd3cb
Unknown <stephane.viper@gmail.com>
2020-03-30 19:11:44 +0200

    step 2

--- www/index.js
+++ www/index.js
@@ -1,1 +1,60 @@
-import { Todos } from "wasm-todo-list";
+import { Todos, Todo } from "wasm-todo-list";
+
+class TodoComponent extends HTMLElement {
+  static get tag() {
+    return "todo-component";
+  }
+
+  static get observedAttributes(){
+    return ["text"];
+  }
+
+  constructor() {
+    super();
+
+    this.attachShadow({mode: "open"});
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        .card {
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+          transition: 0.3s;
+          width: 30vw;
+          display: flex;
+          margin: 10px;
+          justify-content:space-between;
+        }
+        .card:hover {
+          box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        }
+        .container {
+          padding: 2px 16px;
+        }
+      </style>
+
+
+      <div class="card">
+        <p>${this.getAttribute('text')}</p>
+        <button type="button" done>Remove</button>
+      </div>
+    `;
+
+    this.doneBtn = this.shadowRoot.querySelector("[done]");
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('Name ', name, oldValue, newValue);
+    if(name === "text") {
+      this.shadowRoot.innerHTML = this.shadowRoot.innerHTML.replace(oldValue, newValue);
+      this.doneBtn = this.shadowRoot.querySelector("[done]");
+    }
+  }
+
+  connectedCallback() {
+    this.doneBtn.addEventListener("click", () => {
+      // Todo call Rust method
+    });
+  }
+}
+
+customElements.define(TodoComponent.tag, TodoComponent);
